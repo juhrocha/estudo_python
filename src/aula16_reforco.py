cc3eab77f671d0b6278a198cb2aa1b693e4c465a
Juliana <93.julianarocha@gmail.com>
2020-12-16 20:39:54 -0300

    reforço de tupla

---
+++ src/aula16_reforco.py
@@ -0,0 +1,11 @@
+alimento=('hamburguer','suco','pizza','café','pão','cenoura')
+for comida in alimento:
+    print(f'Eu gosto de {comida}')
+print(alimento[1])
+print(alimento[-2])
+print(alimento[1:3])
+print(alimento[0])
+print(alimento[0:])
+print(alimento[:-2])
+print(alimento[-2:])
+print(sorted(alimento))
