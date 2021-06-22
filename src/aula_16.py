7a9a986f2fea14ba0926d340673adb3f446ebf1d
juliana <93.julianarocha@gmail.com>
2021-06-21 21:55:48 -0300

    revisão

--- src/aula_16.py
+++ src/aula_16.py
@@ -9,6 +9,6 @@ for bicho in animal:
 #nesse caso não é necessário usar "in range xxx"
 #OUTRA FORMA DE USO DO FOR:
 #for bicho in range (0,len(animal)):
-#   print(f'Que {animal[bicho]} fofo!)
-    print(f'Que {bicho} fofo!')
+    print(f'Que {animal[1]} fofo!') # aqui o print com [] dá o animal na posição 2
+#    print(f'Que {bicho[2]} fofo!') # aqui o [] exibe a 2 posição do bicho
 print('FIM')
