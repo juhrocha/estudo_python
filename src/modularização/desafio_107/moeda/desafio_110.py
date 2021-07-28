e9f051ab78aa13cb1db8f58a6d7999a5ad8391ad
juliana <93.julianarocha@gmail.com>
2021-07-27 22:32:19 -0300

    + exercicios modularização

---
+++ src/modularização/desafio_107/moeda/desafio_110.py
@@ -0,0 +1,5 @@
+import moeda_2
+
+preco = float(input('Digite um valor: R$ '))
+moeda_2.resumo(preco, 100, 50)
+
--- src/modularização/desafio_107/moeda/moeda_2.py
+++ src/modularização/desafio_107/moeda/moeda_2.py
@@ -22,3 +22,19 @@ def metade(valor, formatado=False):
     half = valor/2
     return half if formatado is False else moeda(half)
 
+
+def titulo(txt):
+    print('*'*30)
+    print(txt.center(30))
+    print('*' * 30)
+
+
+def resumo(valor, aumento=0, diminuicao=0):
+    titulo(f'RESUMO DO VALOR')
+    print(f'Preço analisado: \t{moeda(valor)}')
+    print(f'Dobro do preço: \t{dobro(valor, True)}')
+    print(f'Metade do preço: \t{metade(valor, True)}')
+    print(f'{aumento}% de aumento: \t{aumentar(valor, aumento, True)}')
+    print(f'{diminuicao}% de redução: \t{diminuir(valor, diminuicao, True)}')
+    print('*'*30)
+
