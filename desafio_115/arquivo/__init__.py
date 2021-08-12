0d864033eec24246c6be2b368951739a6b4d4d55
juliana <93.julianarocha@gmail.com>
2021-08-12 00:24:47 -0300

    finalização exercicio 115

--- desafio_115/arquivo/__init__.py
+++ desafio_115/arquivo/__init__.py
@@ -1,8 +1,48 @@
-# def arquivoExiste(nome):
-#     try:
-#         a = open(nome, 'rt')
-#         a.close()
-#     except FileNotFoundError:
-#         return False
-#     else:
-#         return True
+# Como abrir arquivo e exibir um arquivo para leitura
+# a = open('./exercicio_115b.txt', 'rt')
+# for line in a:
+#     print(line)
+
+arq = '/home/juliana/Project/estudo_python/desafio_115/arquivo/exercicio_115b.txt'
+
+
+def arquivoExiste(arquivo):
+    try:
+        leitura = open(arquivo, 'rt')
+        leitura.close()
+    except:
+        return False
+    else:
+        return True
+
+
+def criarArquivo(arquivo):
+    try:
+        leitura = open(arquivo, 'rt')
+    except (FileNotFoundError, FileExistsError):
+        leitura = open(arquivo, 'wt+')
+    else:
+        for line in leitura:
+            print(line)
+
+
+# criarArquivo('Juliana.txt')
+
+
+def abreArquivo(arquivo):
+    leitura = open(arquivo, 'rt')
+    for line in leitura:
+        print(line)
+
+
+# abreArquivo('exercicio_115b.txt')
+
+
+def cadastroArquivo(arquivo, txt):
+    leitura = open(arquivo, 'a')
+    leitura.write(txt)
+    leitura.write('\n')
+    leitura.close()
+
+
+# cadastroArquivo('Ju, 24')
--- desafio_115/teste.py
+++ desafio_115/teste.py
@@ -1,33 +1,34 @@
 from desafio_115 import desafio115
 from time import sleep
-# from desafio_115 import arquivo
-#
-# arqv = 'cursoemvideo.txt'
-#
-# if arquivo.arquivoExiste(arqv):
-#     print('Arquivo encontrado com sucesso!')
-# else:
-#     print('Arquivo não encontrado')
+from desafio_115 import arquivo
+
+arq = '/home/juliana/Project/estudo_python/desafio_115/arquivo/exercicio_115b.txt'
+# arq = 'testanto.txt'
+
+if not arquivo.arquivoExiste(arq):
+    arquivo.criarArquivo(arq)
 
 while True:
-    sleep(0.75)
+    sleep(1)
     resposta = desafio115.menu(['Ver pessoas cadastradas', 'Cadastrar nova pessoa', 'Sair do sistema'])
     if resposta == 1:
         desafio115.linha()
-        sleep(0.75)
+        sleep(1)
         desafio115.titulo('OPÇÃO 1')
+        arquivo.abreArquivo(arq)
         continue
     if resposta == 2:
-        sleep(0.75)
+        sleep(1)
         desafio115.linha()
         desafio115.titulo('OPÇÃO 2')
+        dado = str(input('Digite os dados: '))
+        arquivo.cadastroArquivo(arq, dado)
         continue
     if resposta == 3:
-        sleep(0.5)
+        sleep(0.75)
         print('Saindo...Volte sempre!')
         break
     else:
         sleep(0.75)
         desafio115.linha()
         desafio115.titulo('OPÇÃO INVÁLIDA! Digite um número válido.')
-
