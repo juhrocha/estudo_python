19d8c21871574a327d1c0516b53d707961014c76
juliana <93.juliana.rocha@gmail.com>
2026-05-03 18:37:24 -0300

    - Anotações da aula teórica de POO; - Execução prática da aula de herança em POO, com e sem modularização.

---
+++ aulas_poo/aula_09_modulos.py
@@ -0,0 +1,39 @@
+class Pessoa:
+    #Superclasse
+    def __init__(self, nome = '', idade = 0):
+        self.nome = nome
+        self.idade = idade
+
+    def fazer_aniversario(self):
+        self.idade += 1
+
+
+class Aluno(Pessoa):
+    #Para associar a superclasse à subclasse, basta inserir (nome da classe) e a associação está feita!
+    def __init__(self, nome, idade, curso, turma):
+        super().__init__(nome, idade)
+        self.curso = curso
+        self.turma = turma
+
+    def fazer_matricula(self):
+        print(f'O aluno {self.nome} acabou de fazer matrícula')
+
+
+class Professor(Pessoa):
+    def __init__(self, nome, idade, especialidade, nivel):
+        super().__init__(nome, idade)
+        self.especialidade = especialidade
+        self.nivel = nivel
+
+    def dar_aula(self):
+        print(f'Prof. {self.nome} começou a dar aula')
+
+
+class Funcionario(Pessoa):
+    def __init__(self, nome, idade, cargo, setor):
+        super().__init__(nome, idade)
+        self.cargo = cargo
+        self.setor = set
+
+    def bater_ponto(self):
+        print(f'{self.nome} acabou de bater o ponto')
